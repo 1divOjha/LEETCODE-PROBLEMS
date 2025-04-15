bc99e3d270aa8f8693f62df36236e349cd56ded3
1divOjha <137999685+1divOjha@users.noreply.github.com>
2025-04-15 10:16:17 +0530

    Time: 2 ms (99.61%) | Memory: 44.9 MB (70.04%) - LeetSync

--- 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.java
+++ 902-minimum-number-of-refueling-stops/minimum-number-of-refueling-stops.java
@@ -1,41 +1,50 @@
 class Solution {
     public int minRefuelStops(int target, int startFuel, int[][] stations) {
         
-    PriorityQueue<Integer> pq = new  PriorityQueue<>(Collections.reverseOrder());
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-    int gasstopcount=0;
-    int fuelleft =startFuel;
-    int i =0;
+        int prevstation=0; 
+        int gasststops=0;
+        int currfuel = startFuel;
 
+        // for each loop so station[0] = dist , station[1] = gas at gastation;
+        for(int[]station : stations){
+            //dist between current and prev station, will be subtracyed from currfuel
+            currfuel-= (station[0] - prevstation);
 
-    //bcoz if fuel left is equal or more than target, then no need to stop to refuel, so we reached target, direct 
-    while( fuelleft < target){
+            // until fuel is more than zero, in this inner while loop
+            while(currfuel<0){
+                if(maxHeap.isEmpty()){
+                    return -1;
+                }
+                //adding from past stations only
+                currfuel += maxHeap.poll();
+                gasststops++;
+            }
 
-        // we want to update heap here, till stations left(i) , and fuelleft is more than equal to dist of current station
-        //so we did && here, if either condition is not met, we check beffore polling that is pq empty, if yes then return -1,
-        // cuz we dont have fuel
-        //(as we used to update pq whenever we move and had more or equal fuel to cur dist)
-        // and if pq not empty then we poll(), since we have less curr fuel than curr dist, 
-        //so we cant reach currdist with this fuel , we need to refuel, i.e. poll
-        while( i<stations.length && stations[i][0] <= fuelleft ){
-            pq.add( stations[i][1]);
-           // fuelleft -= stations[i][0]; 
-           //unnecessary, at one point the dist will be more than curr fuel , 
-           //then we just need to  refuel, poll() 
-            i++;
+            //fuel more than 0
+            prevstation = station[0];
+            maxHeap.add(station[1]);
         }
 
-        if(pq.isEmpty()){
-            return -1;
+        // updating currfuel, this will tell us if we can reach target without stopping or not
+        currfuel -= (target - prevstation);
+
+
+        // for last stop from target, this loop means we cant reach target without making a stop, bt if heap empty, so no fuel left, 
+        //otherwise we refuel until there is fuel in heap, and keep uupdating the stops
+        while( currfuel<0){
+            //no fuel from gasstatiosn left
+            if(maxHeap.isEmpty()){
+                    return -1;
+                } 
+
+            currfuel += maxHeap.poll();
+                gasststops++;
         }
 
-        fuelleft += pq.poll();
-        gasstopcount++;
-        
 
-    }
-
-  return gasstopcount;
+    return gasststops;
 
     }
 }
