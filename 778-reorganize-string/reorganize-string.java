d7e0d508f126eb3fbcba3bee5f610f2c35fb87b6
1divOjha <137999685+1divOjha@users.noreply.github.com>
2025-04-16 10:11:19 +0530

    Time: 5 ms (41.87%) | Memory: 42.1 MB (20.48%) - LeetSync

--- 778-reorganize-string/reorganize-string.java
+++ 778-reorganize-string/reorganize-string.java
@@ -1,57 +1,51 @@
 class Solution {
     public String reorganizeString(String s) {
-        //created hashmap to store frequency of characters
-        HashMap<Character, Integer> map = new HashMap<>();
+      // we make hasmap and put all char and freq in it
+      // nxt we also check if max allowed freq , if more than that return ""
+      // nxt we make a pq of map.entry type, with collection.reverseorder 
+      // we  add map.entrySet() to pq;
+      // then until pq is emprt i.e till theres an element with sm freq, we will do this while loop
+      // We call this a Greedy Approach because at each step, the algorithm makes the locally optimal (greedy) choice
+      // — it picks the most frequent character that can be placed safely — with the hope that this leads to a valid and optimal solution overall. 
+      // we greedily select the character with the highest frequency (thanks to the max-heap)
+      // we poll, at first and add the char(with max freq) to the stringbuilder , and the is stored in current variable.
+      // then we check if a prev variable is not null, and has freq >0 , so we add it back to the max heap
+      // if not outside loop, the current reduces freq by one, as already added to the string, and then the current becomes the prev, 
+      // at last we check if the given string length and our string is equal , if yes we return our string, otherwise ""
 
-        int maxFreq = 0;
+      HashMap<Character, Integer> map = new  HashMap<>();
+      int maxFreq= 0;
+       for(char ch : s.toCharArray()){
+        int freq = map.getOrDefault(ch, 0)+1;
+        map.put(ch, freq);
+        maxFreq = Math.min(maxFreq, freq);
 
-        //used fir each loop and updated/put characters in the map along with their frequencies in the string
-        for (char ch : s.toCharArray()) {
-            int Freq = map.getOrDefault(ch, 0) + 1;
-            map.put(ch, Freq);
-            maxFreq = Math.max(maxFreq, Freq);
+       }
+       if (maxFreq > (s.length() + 1) /2){
+        return "";
+       }
+
+       StringBuilder sb = new StringBuilder();
+
+       PriorityQueue<Map.Entry<Character, Integer>> maxheap = new  PriorityQueue<>((a,b) -> b.getValue() - a.getValue());
+       maxheap.addAll(map.entrySet());
+
+       Map.Entry<Character, Integer> prev = null;
+
+       while(!maxheap.isEmpty()){
+        Map.Entry<Character, Integer> curr= maxheap.poll();
+        sb.append(curr.getKey());
+
+        if(prev!= null && prev.getValue() >0){
+            maxheap.add(prev);
         }
 
-        // max allowed freq is (l+1)/2
-        int l = s.length();
-        if (maxFreq > (l + 1) / 2) {
-            return "";
-        }
+        curr.setValue(curr.getValue() -1);
+        prev = curr;
 
-        // maxheap, to store frequency, with custom comparator, and type Map.Entry<char ,int>
-        PriorityQueue<Map.Entry<Character, Integer>> pq = new PriorityQueue<>((a, b) -> b.getValue() - a.getValue());
 
-        //this will generate the max heap 
-        pq.addAll(map.entrySet());
-
-        //prev entry will be stored in this, and if not null, i.e. has freq left after appending( the current becomes prev)
-        // then it will be added back to the heap
-        Map.Entry<Character, Integer> prev = null;
-
-        //char to string builder
-        StringBuilder sb = new StringBuilder();
-
-        //greedy approach,
-        while (!pq.isEmpty()) {
-
-            //max freq char , removed and stored in current
-            Map.Entry<Character, Integer> current = pq.poll();
-            sb.append(current.getKey());
-
-            //in first go its null, so wont enter here, but at the end after updating currents freq, when we put curr in prev, then its no more null, 
-            //and if it has more than 0 freq of the char , then its added bach to the heap, rest char arrangemenet acc to freq pq will handle
-            if (prev != null && prev.getValue() > 0) {
-                pq.add(prev);
-            }
-
-            //updating current map entries value as iyts freq shld be reduced as we added it in to the stringbuilder
-            current.setValue(current.getValue() - 1);
-            //now if current is null, i.e. element has no freq left
-            prev = current;
-
-        }
-
-        return sb.length() == s.length() ? sb.toString() : "";
-
+       }
+       return sb.length()==s.length()?sb.toString():"";
+       
     }
 }
