321dc0fba8a7569de1d2998b50e745c3ac85ce10
Divya Ojha <divyaojha792@gmail.com>
2025-07-26 00:04:14 +0530

    Time: 8 ms (85.80%) | Memory: 47 MB (15.97%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,28 +1,45 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-         List<int[]> merged = new ArrayList<>();
+
+        //a 2d array given , each row has 2 elements, start and end. 
         int n = intervals.length;
+        //we are sorting arr by 0th index of every element, i.e by start time, in ascending order
+        Arrays.sort(intervals, (a,b)-> a[0] -b[0]);
+        if(n<2){ 
+        return intervals; }
+
+        int res =0;
         
-           if(n==0)return new int [0][0];
-        
-      
-        
-        Arrays.sort(intervals, (a,b)-> a[0]-b[0]);
-          int[] current = intervals[0];
         for(int i =1; i<n; i++){
-            if(current[1] >= intervals[i][0] ){
-                current[1]= Math.max(current[1], intervals[i][1]);
+            //if prev [i-1][1] element>= next [i][0] element, update current [i][1]
+            //we enter if, means merging intervals.
+            if(intervals[res][1] >= intervals[i][0]){
+                //we updated the end time for the res index element, which ever was max, 
+                //as it may be the next ele ends early than prev element
+             intervals[res][1]= Math.max( intervals[res][1], intervals[i][1]);
+             // and notice we are not updating the res here, as there may be more next pairs that can be merged with it,
+             // so we cant update res. we only update res, when nonoverlapping pairs, it can also be basically count of non overlapping pairs
             }
 
             else{
-               merged.add(current);
-                current= intervals[i];
+                //non overlapping pair, so we ll put current i's , valye at res index
+                res++;
+               
+                intervals[res][0]=intervals[i][0];
+                intervals[res][1]=intervals[i][1];
             }
         }
+            int[][] ans = new int[res+1][2];
+            for(int i=0; i<=res; i++){
+                ans[i][0]= intervals[i][0];
+                ans[i][1]= intervals[i][1];
+            }
 
-       merged.add(current);
+        // now we need to return an array of the non-overlapping intervals 
         
+    
 
-        return merged.toArray(new int[merged.size()][]);
+     return ans;
+
     }
 }
