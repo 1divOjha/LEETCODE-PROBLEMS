3a321c02dc03628dbaa34602482855ece6319c28
Divya Ojha <divyaojha792@gmail.com>
2025-07-26 00:48:16 +0530

    Time: 64 ms (22.29%) | Memory: 61.6 MB (28.90%) - LeetSync

--- 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/kth-largest-element-in-an-array.java
@@ -1,27 +1,17 @@
 class Solution {
     public int findKthLargest(int[] nums, int k) {
-//         PriorityQueue<Integer> pq = new  PriorityQueue<>(Collections.reverseOrder());
-//         for(int num : nums){
-//             pq.add(num);
-//         } 
-// int ans =0;
-//         while(k>0){
-//            ans= pq.poll();
-//             k--;
-//         }
-//         return ans;
-
-
-//method 2 - t.c : to build pq - reduced fron nlogn to nlogk; by keeping only k elements in pq
-// now using minimum(by default) pq, 
-PriorityQueue<Integer> pq = new PriorityQueue<>(); // min heap by default, has smallest element at node
-int n = nums.length;
-for(int i =0; i<n ; i++){
-    pq.add(nums[i]);
-    if(pq.size()>k){
-        pq.poll();
+    PriorityQueue <Integer> pq = new PriorityQueue<>();
+    //collections.reverseOrder() makes it max heap, its min heap by default
+    int n = nums.length;
+    for(int i =0; i<n; i++){
+        pq.add(nums[i]);
+        if(pq.size()>k){
+            pq.poll();
+        }
     }
-}
-return pq.peek();
+
+ return pq.peek();
+
+
     }
 }
