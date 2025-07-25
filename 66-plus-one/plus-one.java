98d2e799eb7df8eaf43d6ddf5940827a5079fec3
Divya Ojha <divyaojha792@gmail.com>
2025-07-26 00:37:46 +0530

    Time: 0 ms (100.00%) | Memory: 41.6 MB (92.92%) - LeetSync

--- 66-plus-one/plus-one.java
+++ 66-plus-one/plus-one.java
@@ -1,20 +1,17 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-       int n = digits.length;
-       for(int i =n-1; i>=0 ; i--){
-        if(digits[i]<9){
-            digits[i]++;
-            return digits;
+        int n = digits.length;
+        for(int i = n-1; i>=0; i--){
+            if(digits[i]<9){
+                digits[i]++;
+                return digits;
+            }
+            //we gotta put 0, in place if the digit is 9
+            digits[i]=0;
         }
-        //whenever digit is 9, we put zero in its place
-        digits[i]=0;
-       }
 
-
-//if reached here, means all digits are 9, so we need bigger array, by one size
-int[] ans = new int[n+1];
-ans[0]=1;
-return ans;
-    
+        int[]ans = new int[n+1];
+        ans[0]=1;
+        return ans;
     }
 }
