fbc0a1a97b929234ca16f5d9fd87f5996bdb7062
Divya Ojha <divyaojha792@gmail.com>
2025-08-01 14:11:24 +0530

    Time: 8 ms (2.04%) | Memory: 42.5 MB (8.22%) - LeetSync

--- 1848-sum-of-unique-elements/sum-of-unique-elements.java
+++ 1848-sum-of-unique-elements/sum-of-unique-elements.java
@@ -1,21 +1,32 @@
 class Solution {
     public int sumOfUnique(int[] nums) {
 
-int sum=0;
-HashMap<Integer, Integer> map = new HashMap<>();
-//frequency map, stores, num as index and its count as value;
-//map.getValue(),  map.getKey(); map.getOrDefault();
-for(int num: nums){
-    map.put(num, map.getOrDefault(num,0) +1);
-   //stored all num, and their occurances 
-}
 
-for( Map.Entry<Integer, Integer> entry: map.entrySet()){ //we just mention map here, bs inside loop, entry will do the work
-    if(entry.getValue()==1){
-        sum+= entry.getKey();
-    }
-}
-return sum;
+int result = Arrays.stream(nums)
+    .boxed()
+    .collect(Collectors.groupingBy(n -> n, Collectors.counting()))
+    .entrySet().stream()
+    .filter(e -> e.getValue() == 1)
+    .mapToInt(Map.Entry::getKey)
+    .sum();
+    return result;
+
+        //correct runs
+// int sum=0;
+// HashMap<Integer, Integer> map = new HashMap<>();
+// //frequency map, stores, num as index and its count as value;
+// //map.getValue(),  map.getKey(); map.getOrDefault();
+// for(int num: nums){
+//     map.put(num, map.getOrDefault(num,0) +1);
+//    //stored all num, and their occurances 
+// }
+
+// for( Map.Entry<Integer, Integer> entry: map.entrySet()){ //we just mention map here, bs inside loop, entry will do the work
+//     if(entry.getValue()==1){
+//         sum+= entry.getKey();
+//     }
+// }
+// return sum;
 
 
         // this is fine, but doesnt handle duplicates well, only removes duplicate on 2nd occurance, and adds them on 1st and 3 rd occurance
