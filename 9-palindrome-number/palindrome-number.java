9b2e8a0567a39987e1c996643ec3bbbcd9d84dd1
Divya Ojha <divyaojha792@gmail.com>
2025-08-05 19:58:08 +0530

    Time: 7 ms (13.88%) | Memory: 44 MB (75.99%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -5,27 +5,29 @@ class Solution {
 
 
 
-         if (x < 0) return false;  // Negative numbers are not palindromes
+        //  if (x < 0) return false;  // Negative numbers are not palindromes
 
-        int original = x;
-        int reversed = 0;
+        // int original = x;
+        // int reversed = 0;
 
-        while (x != 0) {
-            int rem = x % 10;
-            x = x / 10;
-            reversed = reversed * 10 + rem;
-        }
+        // while (x != 0) {
+        //     int rem = x % 10;
+        //     x = x / 10;
+        //     reversed = reversed * 10 + rem;
+        // }
 
-        return original == reversed;
+        // return original == reversed;
 
-        
-    //     int rem=0; int ans=x;
-    // StringBuilder() s= new StringBuilder();
-    //     while (rem!=0) {
-    //         rem= ans % 10;
-    //         ans= ans/10;
+       //************************************************************************************ */ 
+        int rem=0; int ans=x;
+        //to convert a number into string
+        String originalstr = String.valueOf(x);
+        StringBuilder sbuilder= new StringBuilder(originalstr);
+        String reversestr= sbuilder.reverse().toString(); 
+        //we need to convert the sbuilder into a string first
 
-        
+        return (originalstr.equals(reversestr));
+        //i used equals bcoz it compares content of string objects,  as "==" operand compares references not the content
 
     }
 }
