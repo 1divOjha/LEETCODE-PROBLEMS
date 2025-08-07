eba523cf8849e71fb0d46be0e98d8d06bd7e4d8d
Divya Ojha <divyaojha792@gmail.com>
2025-08-07 20:35:57 +0530

    Time: 0 ms (100.00%) | Memory: 41.7 MB (45.53%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,25 +1,40 @@
 class Solution {
    
     public char repeatedCharacter(String s) {
+        //all three accepted
 
-        boolean seen[] = new boolean[26]; // only lowercase- its mentioned in the question
-        for(char c: s.toCharArray()){
-            if(seen[ c -'a']){ 
-            // i.e if seen already, as soon as we see an element 
-            //again(i.e leftmost 2nd occurance of a character), we return the char
-            return c;
+        int [] arrayocc= new int[26];
+        Arrays.fill(arrayocc,-1);
+        for(int i=0; i<s.length(); i++){
+            int val= arrayocc[ s.charAt(i) -'a'];
+            if(val==-1){
+                arrayocc[ s.charAt(i) -'a']=i;
             }
-
-            else{ //not seen till now, but saw now so make it true
-            seen[c-'a']=true;
-
+            else{
+                return s.charAt(i);
             }
         }
-        
-        //the sum has said that it has atleast one repeating char, so shouldnt reach here
+
         return '\0';
 
+    //**************************************************************************** */
+        // boolean seen[] = new boolean[26]; // only lowercase- its mentioned in the question
+        // for(char c: s.toCharArray()){
+        //     if(seen[ c -'a']){ 
+        //     // i.e if seen already, as soon as we see an element 
+        //     //again(i.e leftmost 2nd occurance of a character), we return the char
+        //     return c;
+        //     }
 
+        //     else{ //not seen till now, but saw now so make it true
+        //     seen[c-'a']=true;
+
+        //     }
+        // }
+        // //the sum has said that it has atleast one repeating char, so shouldnt reach here
+        // return '\0';
+
+/************************************************************************************************************************** */
                 // int ans = Integer.MAX_VALUE;
         // for(int i =0; i<s.length() ; i++){
         //     for(int j =i+1; j<s.length(); j++){
