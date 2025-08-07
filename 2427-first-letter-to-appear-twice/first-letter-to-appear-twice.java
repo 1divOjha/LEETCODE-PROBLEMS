1a72fc3f5610980f7ad1ba010d88616eda749cf5
Divya Ojha <divyaojha792@gmail.com>
2025-08-07 20:29:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (81.69%) - LeetSync

--- 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
+++ 2427-first-letter-to-appear-twice/first-letter-to-appear-twice.java
@@ -1,16 +1,33 @@
 class Solution {
+   
     public char repeatedCharacter(String s) {
-        int ans = Integer.MAX_VALUE;
-        for(int i =0; i<s.length() ; i++){
-            for(int j =i+1; j<s.length(); j++){
-                if(s.charAt(i)==s.charAt(j)){
-                    ans =Math.min(ans, j);
-                }
+
+        boolean seen[] = new boolean[26]; // only lowercase- its mentioned in the question
+        for(char c: s.toCharArray()){
+            if(seen[ c -'a']){ 
+            // i.e if seen already, as soon as we see an element 
+            //again(i.e leftmost 2nd occurance of a character), we return the char
+            return c;
+            }
+
+            else{ //not seen till now, but saw now so make it true
+            seen[c-'a']=true;
+
             }
         }
+        
+        //the sum has said that it has atleast one repeating char, so shouldnt reach here
+        return '\0';
 
-        return s.charAt(ans);
 
-
+                // int ans = Integer.MAX_VALUE;
+        // for(int i =0; i<s.length() ; i++){
+        //     for(int j =i+1; j<s.length(); j++){
+        //         if(s.charAt(i)==s.charAt(j)){
+        //             ans =Math.min(ans, j);
+        //         }
+        //     }
+        // }
+        // return s.charAt(ans);
     }
 }
