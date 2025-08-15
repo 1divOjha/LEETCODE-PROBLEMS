fe24dea2a5d933925ae1cff0a0ea086529f1aba6
Divya Ojha <divyaojha792@gmail.com>
2025-08-15 23:42:14 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (56.73%) - LeetSync

--- 908-middle-of-the-linked-list/middle-of-the-linked-list.java
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.java
@@ -11,22 +11,34 @@
 class Solution {
     
     public ListNode middleNode(ListNode head) {
-        //do zero based indexing
-        ListNode middle = head;
-        if(head.next==null){
-            return head;
-        }
-        int i =0;
+
+        //using slow fast pointer
+        ListNode slow = head;
+        ListNode fast =head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        } 
+return slow; // this will be at the middle, after coming out of while loop
+
+    }}
+
+        //do zero based indexing - did the sum myselffff********************************
+//         ListNode middle = head;
+//         if(head.next==null){
+//             return head;
+//         }
+//         int i =0;
         
-        while(middle!=null){
-            middle = middle.next;
-            i++;
-        }
-        int pos = i/2;
-        for(int j =0; j<pos; j++){
-         head = head.next ;
+//         while(middle!=null){
+//             middle = middle.next;
+//             i++;
+//         }
+//         int pos = i/2;
+//         for(int j =0; j<pos; j++){
+//          head = head.next ;
             
-        }
-        return head;
-    }
-}
+//         }
+//         return head;
+//     }
+// }
